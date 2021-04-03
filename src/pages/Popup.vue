4ab67f2688b89f6f04464abf0f2451bbebbbb503
Simon Cleriot <cleriotsimon@gmail.com>
2021-04-03 13:59:00 +0200

    Screenshot link in extension popup

--- src/pages/Popup.vue
+++ src/pages/Popup.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>Test</div>
+  <div>
+    <button @click="openScreenshotsList" class="bg-blue-500 rounded-lg font-bold text-sm text-white text-center px-4 py-2 transition duration-300 ease-in-out hover:bg-blue-600 ml-3">Screenshots</button>
+  </div>
 </template>
 
 <script>
-export default {
+import browser from "webextension-polyfill"
 
+export default {
+  methods: {
+    openScreenshotsList() {
+      window.open(browser.extension.getURL("build/pages/index.html")+"#/screenshots");
+    }
+  }
 }
 </script>
 
